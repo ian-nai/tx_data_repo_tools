e993064793ced560a356885551d7411be361b77e
Ian <iangoodale@utexas.edu>
2021-05-15 08:31:54 -0500

    Update date_range_search.py

--- date_range_search.py
+++ date_range_search.py
@@ -6,35 +6,14 @@ base = 'https://dataverse.tdl.org'
 rows = 10
 start = 0
 page = 1
-condition = True # emulate do-while
-
-
-# API: 7fa85ece-2728-459f-abc6-d1b9428e6127
-'''
-while (condition):
-
-    url = base + '/api/search?q=tree' + "&start=" + str(start)
-    resp = requests.get(url, headers={"X-Dataverse-key":"7fa85ece-2728-459f-abc6-d1b9428e6127"})
-    data = resp.json()
-    total = data['data']['total_count']
-    print("=== Page", page, "===")
-    print("start:", start, " total:", total)
-    for i in data['data']['items']:
-        print("- ", i['name'], "(" + i['type'] + ")")
-    start = start + rows
-    page += 1
-    condition = start < total
-'''
+condition = True 
 
 
 while (condition):
 
     file_names = []
-
-    #url = base + '/api/search?q=tree' + "&start=" + str(start)
     url = 'https://dataverse.tdl.org/api/search?q=*&per_page=1000&sort=date&order=asc&q=*&fq=dateSort:[2021-05-01T00:00:00Z+TO+2021-05-09T00:00:00Z]'
-
-    resp = requests.get(url, headers={"X-Dataverse-key":"7fa85ece-2728-459f-abc6-d1b9428e6127"})
+    resp = requests.get(url, headers={"X-Dataverse-key":"[YOUR KEY]"})
     data = resp.json()
     total = data['data']['total_count']
     print("=== Page", page, "===")
@@ -48,48 +27,29 @@ while (condition):
 
 
 
-    smallerlist = [l.split(',') for l in ','.join(file_names).split('(dataset)')]
-    print(smallerlist)
-
+    split_list = [l.split(',') for l in ','.join(file_names).split('(dataset)')]
+    
 dataset_list = []
-
-# for x in file_names:
-#     if '(dataset)' in x:
-#         dataset_list.append(x)
-
-for y in smallerlist:
+for y in split_list:
     for x in y:
         if '(file)' not in x:
             y.remove(x)
             dataset_list.append(x)
 
-dataset_list2 = [x for x in dataset_list if x != []]
-smallerlist2 = [x for x in smallerlist if x != []]
+dataset_list_cleaned = [x for x in dataset_list if x != []]
+final_list = [x for x in split_list if x != []]
 
-dataset_list3 = []
-for item in dataset_list2:
+dataset_list_final = []
+for item in dataset_list_cleaned:
     if item != ' ':
-        dataset_list3.append(str(item))
-
-
-print(dataset_list3)
-
-
-final_list = []
-q = len(smallerlist)
+        dataset_list_final.append(str(item))
 
 count = 0
-for x in smallerlist2:
-    x.insert(0,dataset_list3[count])
+for x in final_list:
+    x.insert(0,dataset_list_final[count])
     count += 1
 
-# smallerlist3 = []
-# for lists in smallerlist2:
-#     for item in lists:
-#         if item:
-#             smallerlist3.append(item)
-
-print(smallerlist2)
+print(final_list)
 
 import xlsxwriter
 
@@ -98,11 +58,10 @@ worksheet = workbook.add_worksheet()
 
 worksheet.set_column('A:A', 14)
 
-#cell_format = workbook.add_format()
 format1 = workbook.add_format({'bg_color': '#FFC7CE',
                                'font_color': '#9C0006'})
 
-for row, row_data in enumerate(smallerlist2):
+for row, row_data in enumerate(final_list):
     worksheet.write_row(row + 1, 1, row_data)
 
 worksheet.conditional_format('A2:K7', {'type': 'text',
@@ -122,13 +81,4 @@ import csv
 
 with open("out.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerows(smallerlist2)
-
-
-
-# https://guides.dataverse.org/en/latest/api/search.html#
-
-# basically: search for dataverses
-# iterate through dataverses to get file types
-# from there, determine number of file infractions
-# could do only dataverses within certain time frame, etc.
+    writer.writerows(final_list)
