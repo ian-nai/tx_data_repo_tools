48111f7b835e08eb9c355913deae6db2a704c40b
Ian <iangoodale@utexas.edu>
2021-12-17 09:09:19 -0600

    Create convert_and_delete.py

---
+++ convert_and_delete.py
@@ -0,0 +1,110 @@
+import glob
+import pandas as pd
+from docx2pdf import convert
+import os
+import subprocess
+import pathlib
+from pathlib import Path
+import shutil
+
+types = ['**/*.doc', '**/*.docx', '**/*.psd', '**/*.mov', '**/*.accdb', '**/*.xlsx', '**/*.xls', '**/*.xlsm', '**/*.ppt', '**/*.pptx', '**/*.wmv', '**/*.wma', '**/*.mp3', '**/*.wpl', '**/*.R', '**/*.py']
+
+files_to_delete = []
+
+def check_docu():
+
+        files_checked = []
+
+        for type in types:
+            for file in glob.glob(type, recursive=True):
+                print(file)
+                files_checked.append(file)
+
+        files_checked = [file for file in files_checked if not file.startswith('venv')]
+
+        print(files_checked)
+
+        for file in files_checked:
+            files_to_delete.append(file)
+
+
+        if len(files_checked) >= 1:
+            num_files = len(files_checked)
+            print(str(num_files) + ' files to be changed!')
+            cont = input("Convert all files? y/n  ")
+            if cont == 'y':
+                convert_files(files_checked)
+            if cont == 'n':
+                print('okay')
+            elif cont != 'y' and cont != 'n':
+                print('Please type y or n.')
+
+        if len(files_checked) == 0:
+            print('No files to convert.')
+
+def convert_files(files_checked):
+
+        cur_full_path = os.path.abspath(os.getcwd())
+
+        csv_matchers = ['xls','xlsx']
+        csv_matching = [s for s in files_checked if any(xs in s for xs in csv_matchers)]
+        if len(csv_matching) >= 1:
+            for x in csv_matching:
+                data_xls = pd.read_excel(x, index_col=None)
+                data_xls.to_csv((x + '.csv'), encoding='utf-8', index=False)
+
+        doc_matchers = ['doc','docx']
+        doc_matching = [s for s in files_checked if any(xs in s for xs in doc_matchers)]
+        if len(doc_matching) >= 1:
+            for x in doc_matching:
+                 convert(x, (x + '.pdf'))
+
+        mov_matchers = ['mov']
+        mov_matching = [s for s in files_checked if any(xs in s for xs in mov_matchers)]
+        if len(mov_matching) >= 1:
+            for x in mov_matching:
+                 subprocess.Popen(['ffmpeg', '-i', x, '-vcodec', 'h264', '-acodec', 'aac', (x + '.mp4')])
+
+        sound_matchers = ['wma', 'mp3']
+        sound_matching = [s for s in files_checked if any(xs in s for xs in sound_matchers)]
+        if len(sound_matching) >= 1:
+            for x in sound_matching:
+                subprocess.Popen(['ffmpeg', '-i', x, (x + '.wav')])
+
+        ppt_matchers = ['ppt', 'pptx']
+        ppt_matching = [s for s in files_checked if any(xs in s for xs in ppt_matchers)]
+        if len(ppt_matching) >= 1:
+            for x in ppt_matching:
+                lib_dir = '/Applications/LibreOffice.app/Contents/MacOS'
+                if os.path.exists(lib_dir) and os.path.isdir(lib_dir):
+                    os.chdir(lib_dir)
+                    subprocess.Popen(['./soffice',  '--headless', '--invisible', '--convert-to', 'pdf', '--outdir', cur_full_path, cur_full_path])
+
+        r_matchers = ['R']
+        r_matching = [s for s in files_checked if any(xs in s for xs in r_matchers)]
+        if len(r_matching) >= 1:
+                 for r in r_matching:
+                     try:
+                         print(r)
+                         subprocess.check_output(["sudo", "R", "CMD", "check", r])
+                     except subprocess.CalledProcessError as e:
+                         continue
+
+
+        py_matchers = ['py']
+        py_matching = [s for s in files_checked if any(xs in s for xs in py_matchers)]
+        if len(py_matching) >= 1:
+                 for p in py_matching:
+                     try:
+                         print(p)
+                         subprocess.check_output(['python3', '-m', 'py_compile', p])
+                     except subprocess.CalledProcessError as e:
+                         continue
+
+        remove_old_formats()
+
+def remove_old_formats():
+    for file in files_to_delete:
+        os.remove(file)
+
+check_docu()
